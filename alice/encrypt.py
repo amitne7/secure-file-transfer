1829e3361547da8f2b5a3afde80c020dae03ef94
Amit Baloda <amit.baloda09@gmail.com>
2025-09-02 20:12:32 +0100

    update encryption and decryption

--- alice/encrypt.py
+++ alice/encrypt.py
@@ -1,35 +1,86 @@
-from Crypto.PublicKey import RSA
-from Crypto.Hash import SHA256
-from Crypto.Signature import PKCS1_v1_5
-image_file = 'confidential_image.JPG'
+from Crypto.Hash import SHA3_512
+from Crypto.PublicKey import ECC, RSA
+from Crypto.Signature import DSS
+from Crypto.Random import get_random_bytes
+from Crypto.Cipher import AES, PKCS1_OAEP
+from json import dumps
+from base64 import b64encode
+from Crypto.Util.Padding import pad
+from OpenSSL.crypto import load_certificate, FILETYPE_PEM, X509Store, X509StoreContext
+
+
 file_sent_to_bob = '../bob/encrypted.txt'
 alice_signing_key_file = 'alice_signing_key.pem'
 bob_public_key_file = 'bob_public_key.crt'
+ca_certificate = 'ca.crt'
+image_file = 'confidential_image.JPG'
+password = 'alicesecret'
 
-alice_signing_key_password = b'alicesecret'
 
-with open(alice_signing_key_file, 'rb') as key_pair:
-    private_key = RSA.import_key(key_pair.read(), alice_signing_key_password)
-    pk = private_key.export_key('OpenSSH')
-print(pk)
-# # Open the input file for reading in binary format
-# f_open = open(bob_public_key_file, 'rb')
-# f_key = f_open.read()
-# f_open.close()
-# f = open(image_file, 'rb')
-# input_bytes = f.read()
-# f.close()
-# #print(input_bytes)
-# key_pair = RSA.import_key(f_key)
-# public_key = key_pair.export_key('OpenSSH')
-# print(public_key)
-# if isinstance(key_pair, RSA.RsaKey):
-#     print("RSA public key")
-# else:
-#     print("Other key")
-# # Open the output file for writing in text format
-# #f = open(file_sent_to_bob, 'w+')
-# # Write the local variable output_text to the file
-# #f.write(output_text)
-# # Close the file
-# #f.close()
+def read_file(file_name, mode):
+    with open(file_name, mode) as file_content:
+        return file_content.read()
+
+
+def encryption():
+    alice_signing_key_file_content = read_file(alice_signing_key_file, 'rb')
+    alice_private_key = ECC.import_key(alice_signing_key_file_content, passphrase=password)
+
+    bob_public_key_file_content = read_file(bob_public_key_file, 'rb')
+    bob_public_key = RSA.import_key(bob_public_key_file_content)
+
+    message = read_file(image_file, 'rb')
+
+    # padded plaintext with block size
+    padded_plaintext = pad(message, AES.block_size)
+    session_key = get_random_bytes(32)  # Generate session key of 256 bit
+
+    # encrypt padded plaintext using AES CBC mode
+    cipher_aes = AES.new(session_key, AES.MODE_CBC)
+    ciphertext = cipher_aes.encrypt(padded_plaintext)
+    iv = cipher_aes.iv
+
+    # Encrypt the session key with Bob's public key
+    cipher_rsa = PKCS1_OAEP.new(bob_public_key)
+    encrypted_session_key = cipher_rsa.encrypt(session_key)
+
+    # sign the message with alice private key
+    hash_value = SHA3_512.new(message)  # create hash using SHA3_512
+
+    # crating signing object of DSA using FIPS-186-3 and alice
+    # private key
+    signer = DSS.new(alice_private_key, 'fips-186-3')
+    signature = signer.sign(hash_value)
+
+    # create json string by decoding values with base64
+    json_string = dumps({
+        "signature": b64encode(signature).decode('utf-8'),
+        "key": b64encode(encrypted_session_key).decode('utf-8'),
+        "ciphertext": b64encode(ciphertext).decode('utf-8'),
+        "iv": b64encode(iv).decode('utf-8')
+    })
+    print('File encrypted successfully!')
+    # write json string to file to share with bob
+    with open(file_sent_to_bob, 'w+') as encrypted_file:
+        encrypted_file.write(json_string)
+
+
+# validate issued certificate
+def validate_certificate():
+    root_cert_content = read_file(ca_certificate, 'rb')
+    bob_cert_content = read_file(bob_public_key_file, 'rb')
+
+    root_cert = load_certificate(FILETYPE_PEM, root_cert_content)
+    bob_cert = load_certificate(FILETYPE_PEM, bob_cert_content)
+    store = X509Store()
+    store.add_cert(root_cert)
+    store.add_cert(bob_cert)
+    store_ctx = X509StoreContext(store, bob_cert)
+    try:
+        store_ctx.verify_certificate()
+        encryption()
+    except Exception as e:
+        print('Error', e)
+
+
+validate_certificate()
--- bob/decrypt.py
+++ bob/decrypt.py
@@ -1,26 +1,79 @@
 from Crypto.Cipher import AES, PKCS1_OAEP
-from Crypto.Hash import SHA3_256
+from Crypto.Hash import SHA3_512
 from Crypto.PublicKey import ECC, RSA
 from Crypto.Signature import DSS
 from json import loads
+from base64 import b64decode
+from Crypto.Util.Padding import unpad
+from OpenSSL.crypto import load_certificate, FILETYPE_PEM, X509Store, X509StoreContext
 
 file_received_from_alice = 'encrypted.txt'
 decrypted_image_file = 'image.jpg'
 bob_private_key_file = 'bob_private_key.pem'
 alice_verifying_key_file = 'alice_verifying_key.crt'
+ca_certificates = 'ca.crt'
+password = 'bobsecret'
 
-bob_password = b'bobsecret'
 
-# Open the input file for reading in text format
-f = open(file_received_from_alice, 'r')
-# Read in the file contents and save them in the local variable input_text
-input_text = f.read()
-# Close the file
-f.close()
+def read_file(file_name, mode):
+    with open(file_name, mode) as file_content:
+        return file_content.read()
 
-# Open the output file for writing in binary format
-f = open(decrypted_image_file, 'wb+')
-# Write the local variable file_bytes to the file
-f.write(file_bytes)
-# Close the file
-f.close()
+
+# Decryption using AES using CBC mode
+def decryption():
+    json_string = read_file(file_received_from_alice, 'r')
+    json_object = loads(json_string)
+
+    bob_private_key_file_content = read_file(bob_private_key_file, 'rb')
+    private_key = RSA.import_key(bob_private_key_file_content, passphrase=password)
+
+    alice_verify_key_content = read_file(alice_verifying_key_file, 'rb')
+    alice_verify_key = ECC.import_key(alice_verify_key_content)
+
+    cipher_text = b64decode(json_object['ciphertext'])
+    iv = b64decode(json_object['iv'])
+    encrypted_key = b64decode(json_object['key'])
+    signature = b64decode(json_object['signature'])
+
+    # decrypt encrypted session key using private key
+    cipher_decrypt = PKCS1_OAEP.new(private_key)
+    session_key = cipher_decrypt.decrypt(encrypted_key)
+
+    # decrypt cipher text using AES in CBC mode using aes key and iv
+    cipher_aes = AES.new(session_key, AES.MODE_CBC, iv=iv)
+    padded_plaintext = cipher_aes.decrypt(cipher_text)
+
+    # un pad plaintext
+    plaintext = unpad(padded_plaintext, AES.block_size)
+
+    # get hash value and verifier using alice verifying key
+    hash_value = SHA3_512.new(plaintext)
+    verifier = DSS.new(alice_verify_key, 'fips-186-3')
+    try:
+        verifier.verify(hash_value, signature)
+        print('File decrypted successfully!')
+        with open(decrypted_image_file, 'wb+') as op:
+            op.write(plaintext)
+    except ValueError:
+        print("Invalid signature. data may altered")
+
+
+def validate_certificate():
+    ca_content = read_file(ca_certificates, 'rb')
+    alice_cert_content = read_file(alice_verifying_key_file, 'rb')
+
+    root_cert = load_certificate(FILETYPE_PEM, ca_content)
+    bob_cert = load_certificate(FILETYPE_PEM, alice_cert_content)
+    store = X509Store()
+    store.add_cert(root_cert)
+    store.add_cert(bob_cert)
+    store_ctx = X509StoreContext(store, bob_cert)
+    try:
+        store_ctx.verify_certificate()
+        decryption()
+    except Exception as e:
+        print('Error', e)
+
+
+validate_certificate()
